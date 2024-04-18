a7dcefb2d96abeba07644e85e5abe9e4082aaa0f
Sam Day <me@samcday.com>
2024-04-18 11:45:41 +0200

    phosh-dm: basic (generated) high-level bindings

---
+++ phosh-dm/src/lib.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+/// Asserts that this is the main thread and either `gdk::init` or `gtk::init` has been called.
+macro_rules! assert_initialized_main_thread {
+    () => {
+        if !::gtk::is_initialized_main_thread() {
+            if ::gtk::is_initialized() {
+                panic!("GTK may only be used from the main thread.");
+            } else {
+                panic!("GTK has not been initialized. Call `gtk::init` first.");
+            }
+        }
+    };
+}
+
+mod auto;
+pub use auto::*;
