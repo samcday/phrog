c90e28f3ee42b204ba424127c2c171e54c7f8b23
Sam Day <me@samcday.com>
2024-04-26 15:25:57 +0200

    greetd request/response basics
    holy shit this was some effort for like 5 lines of code. Rust borrow
    rules + GTK/Glib type system semantics = extreme misery

--- phrog/src/lockscreen/imp.rs
+++ phrog/src/lockscreen/imp.rs
@@ -1,23 +1,22 @@
-use std::cell::{OnceCell, RefCell};
-use std::ops::DerefMut;
-use std::os::unix::net::UnixStream;
+use crate::session_object::SessionObject;
+use crate::user_session_page::UserSessionPage;
 use greetd_ipc::codec::SyncCodec;
-use greetd_ipc::Request;
-use gtk::{glib, Widget};
-use gtk::glib::{closure_local, ObjectExt};
+use greetd_ipc::{Request, Response};
+use gtk::glib::{clone, closure_local, ObjectExt};
 use gtk::prelude::*;
 use gtk::subclass::prelude::{ObjectImpl, ObjectImplExt, ObjectSubclass, ObjectSubclassExt};
 use gtk::subclass::widget::WidgetImpl;
-use phosh_dm::LockscreenExt;
+use gtk::{gio, glib, Widget};
 use phosh_dm::subclass::layer_surface::LayerSurfaceImpl;
 use phosh_dm::subclass::lockscreen::LockscreenImpl;
-use crate::session_object::SessionObject;
-use crate::user_session_page::UserSessionPage;
+use phosh_dm::{LockscreenExt, LockscreenPage};
+use std::cell::{OnceCell, RefCell};
+use std::os::unix::net::UnixStream;
+use async_channel::{Receiver, Sender};
 
 #[derive(Default)]
 pub struct Lockscreen {
     user_session_page: OnceCell<UserSessionPage>,
-    greetd_socket: OnceCell<RefCell<UnixStream>>,
 }
 
 #[glib::object_subclass]
@@ -27,11 +26,26 @@ impl ObjectSubclass for Lockscreen {
     type ParentType = phosh_dm::Lockscreen;
 }
 
+fn run_greetd() -> (Sender<Request>, Receiver<Response>) {
+    let (greetd_req_send, greetd_req_recv) = async_channel::bounded::<Request>(1);
+    let (greetd_resp_send, greetd_resp_recv) = async_channel::bounded(1);
+
+    gio::spawn_blocking(move || {
+        let mut sock = UnixStream::connect(std::env::var("GREETD_SOCK").unwrap()).unwrap();
+
+        while let Ok(req) = greetd_req_recv.recv_blocking() {
+            req.write_to(&mut sock).unwrap();
+            greetd_resp_send.send_blocking(Response::read_from(&mut sock).unwrap()).unwrap();
+        }
+    });
+
+    (greetd_req_send, greetd_resp_recv)
+}
+
 impl ObjectImpl for Lockscreen {
     fn constructed(&self) {
         self.parent_constructed();
 
-        self.greetd_socket.set(RefCell::new(UnixStream::connect(std::env::var("GREETD_SOCK").unwrap()).unwrap())).unwrap();
         self.user_session_page.set(UserSessionPage::new()).unwrap();
 
         if let Some(c) = self.obj().carousel() {
@@ -42,6 +56,8 @@ impl ObjectImpl for Lockscreen {
             c.prepend(self.user_session_page.get().unwrap())
         }
 
+        let (greetd_sender, greetd_receiver) = run_greetd();
+
         self.user_session_page.get().unwrap()
             .connect_closure("login", false, closure_local!(@weak-allow-none self as this => move |_: UserSessionPage, username: String, session: SessionObject| {
                 let this = if let Some(this) = this { this } else {
@@ -50,18 +66,19 @@ impl ObjectImpl for Lockscreen {
 
                 this.obj().upcast_ref::<Widget>().set_sensitive(false);
 
-                glib::spawn_future_local(async move {
-                    // TODO: error handling
-                    Request::CreateSession { username }.write_to(this.greetd_socket.get().unwrap().borrow_mut().deref_mut()).unwrap();
-                });
+                glib::spawn_future_local(clone!(@strong greetd_sender, @strong greetd_receiver => async move {
+                    greetd_sender.send(Request::CreateSession { username }).await.unwrap();
+                    let resp = greetd_receiver.recv().await.unwrap();
+
+                    this.obj().upcast_ref::<Widget>().set_sensitive(true);
+                    this.obj().upcast_ref::<phosh_dm::Lockscreen>().set_page(LockscreenPage::Unlock);
+                }));
             }));
     }
 }
 
-impl LayerSurfaceImpl for Lockscreen {
-}
+impl LayerSurfaceImpl for Lockscreen {}
 
 impl WidgetImpl for Lockscreen {}
 
-impl LockscreenImpl for Lockscreen {
-}
+impl LockscreenImpl for Lockscreen {}
