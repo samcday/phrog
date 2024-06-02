126c4138df771cfce82035b59721cba2fa28c417
Sam Day <me@samcday.com>
2024-06-02 16:28:30 +0200

    main: Register resources in phosh_init

--- phosh/src/phosh-main.c
+++ phosh/src/phosh-main.c
@@ -5,12 +5,21 @@
  */
 
 #include "phosh-main.h"
+#include "phosh-resources.h"
+
 #include <handy.h>
 #include <call-ui.h>
 
 void
 phosh_init (void)
 {
+  static gsize resources_initialized = FALSE;
+
+  if (g_once_init_enter (&resources_initialized)) {
+    phosh_register_resource ();
+    g_once_init_leave (&resources_initialized, TRUE);
+  }
+
   hdy_init ();
   cui_init (TRUE);
 }
