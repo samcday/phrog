006d589623e21d2d2e6b7e84801e57624602e398
Sam Day <me@samcday.com>
2024-06-04 17:34:36 +0200

    main: WIP: support multiple gtk::main tests

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ mod test {
     use crate::init;
     use crate::shell::Shell;
     use gtk::glib;
-    use gtk::glib::clone;
+    use gtk::glib::{clone, MainContext};
     use libphosh::prelude::ShellExt;
     use libphosh::prelude::WallClockExt;
     use libphosh::WallClock;
@@ -140,24 +140,53 @@ mod test {
     use greetd_ipc::AuthMessageType::Secret;
     use greetd_ipc::{Request, Response};
     use input_event_codes::*;
-    use std::sync::{Arc, Once};
+    use std::sync::{Arc, OnceLock};
+    use std::sync::mpsc::{channel, Sender};
     use std::time::{Duration, SystemTime, UNIX_EPOCH};
     use wayland_client::Connection;
 
-    static START: Once = Once::new();
-    fn test_setup() {
-        START.call_once(|| {
-            init(None);
+    pub fn run_test<F: FnOnce() + Send + 'static>(func: F) {
+        static START: OnceLock<Sender<(Box<dyn FnOnce() + Send>, Sender<()>)>> = OnceLock::new();
+        let queue = START.get_or_init(|| {
+            let (sender, receiver) = channel::<(Box<dyn FnOnce() + Send>, Sender<()>)>();
+            std::thread::spawn(move || {
+                init(None);
+                while let Ok((func, notify)) = receiver.recv() {
+                    func();
+                    notify.send(()).unwrap();
+                }
+            });
+            sender
         });
+        let (notify, wait) = channel();
+        queue.send((Box::new(func), notify)).unwrap();
+        wait.recv().unwrap();
+    }
+
+    #[test]
+    fn shell_ready() {
+        let ready_called = Arc::new(AtomicBool::new(false));
+
+        run_test(clone!(@strong ready_called => move || {
+            let shell = Shell::new();
+            shell.set_default();
+            let wall_clock = WallClock::new();
+            wall_clock.set_default();
+            shell.set_locked(true);
+            shell.connect_ready(clone!(@strong ready_called => move |_| {
+                ready_called.store(true, Ordering::Relaxed);
+                gtk::main_quit();
+            }));
+            gtk::main();
+        }));
+
+        assert!(ready_called.load(Ordering::Relaxed));
     }
 
     #[test]
     fn test_simple_flow() {
-        test_setup();
-
-        let logged_in = Arc::new(AtomicBool::new(false));
-        // run a fake greetd server
-        std::thread::spawn(clone!(@strong logged_in => move || {
+        run_test(|| {
+            // run a fake greetd server
             let path = temp_dir().join(format!(
                 ".phrog-test-greetd-{}.sock",
                 SystemTime::now()
@@ -165,104 +194,112 @@ mod test {
                     .unwrap()
                     .as_secs()
             ));
-            std::env::set_var("GREETD_SOCK", &path);
             let listener = UnixListener::bind(&path).unwrap();
-            loop {
-                let (mut stream, _addr) = listener
-                    .accept()
-                    .expect("failed to accept greetd connection");
-                match Request::read_from(&mut stream).unwrap() {
-                    Request::CreateSession { .. } => Response::AuthMessage {
-                        auth_message_type: Secret,
-                        auth_message: "Password:".to_string(),
+            std::env::set_var("GREETD_SOCK", &path);
+
+            let shell = Shell::new();
+            let wall_clock = WallClock::new();
+            wall_clock.set_default();
+            shell.set_default();
+            shell.set_locked(true);
+
+            let logged_in = Arc::new(AtomicBool::new(false));
+            let ready_called = Arc::new(AtomicBool::new(false));
+
+            std::thread::spawn(clone!(@strong logged_in => move || {
+                loop {
+                    let (mut stream, _addr) = listener
+                        .accept()
+                        .expect("failed to accept greetd connection");
+                    match Request::read_from(&mut stream).unwrap() {
+                        Request::CreateSession { .. } => Response::AuthMessage {
+                            auth_message_type: Secret,
+                            auth_message: "Password:".to_string(),
+                        }
+                            .write_to(&mut stream)
+                            .unwrap(),
+                        req => panic!("wrong request: {:?}", req),
                     }
-                    .write_to(&mut stream)
-                    .unwrap(),
-                    req => panic!("wrong request: {:?}", req),
+
+                    match Request::read_from(&mut stream).unwrap() {
+                        Request::PostAuthMessageResponse {
+                            response: Some(password),
+                        } => {
+                            assert_eq!(password, "password");
+                            Response::Success.write_to(&mut stream).unwrap();
+                        }
+                        req => panic!("wrong request: {:?}", req),
+                    }
+
+                    match Request::read_from(&mut stream).unwrap() {
+                        Request::StartSession { .. } => {
+                            Response::Success.write_to(&mut stream).unwrap();
+                            logged_in.store(true, Ordering::Relaxed);
+                            return;
+                        }
+                        req => panic!("wrong request: {:?}", req),
+                    }
+                }
+            }));
+
+            let (ready_tx, ready_rx) = async_channel::bounded(1);
+            shell.connect_ready(clone!(@strong ready_called => move |_| {
+                ready_called.store(true, Ordering::Relaxed);
+                ready_tx.send_blocking(()).expect("notify ready failed");
+            }));
+
+            let vp = VirtualPointer::new(Connection::connect_to_env().unwrap());
+            let kb = VirtualKeyboard::new(Connection::connect_to_env().unwrap());
+
+            glib::spawn_future_local(clone!(@strong shell => async move {
+                ready_rx.recv().await.unwrap();
+
+                glib::timeout_future(Duration::from_millis(500)).await;
+
+                // Move the mouse to where the main user selection should be.
+                // TODO: calculate this somehow?
+                // If we had access to the Gtk.Widget ref I think it's straightforward to determine
+                // absolute (screen) coords...
+                let (_, _, width, height) = shell.usable_area();
+                let width = width as u32;
+                let height = height as u32;
+                let x = width / 2;
+                let y = height / 2 - 25; // 25 pixel nudge upwards
+                vp.move_to(x, y, width, height).await;
+
+                // wait a few millis after mouse move - for improved observation in recordings
+                glib::timeout_future(Duration::from_millis(250)).await;
+                // then click the main user
+                vp.click();
+
+                // wait for keypad page to slide in
+                glib::timeout_future(Duration::from_millis(500)).await;
+
+                // type password (uh, literally)
+                let keys = [
+                    KEY_P!(),
+                    KEY_A!(),
+                    KEY_S!(),
+                    KEY_S!(),
+                    KEY_W!(),
+                    KEY_O!(),
+                    KEY_R!(),
+                    KEY_D!(),
+                    KEY_ENTER!(),
+                ];
+                for key in keys {
+                    kb.keypress(key).await;
+                    glib::timeout_future(Duration::from_millis(100)).await;
                 }
 
-                match Request::read_from(&mut stream).unwrap() {
-                    Request::PostAuthMessageResponse {
-                        response: Some(password),
-                    } => {
-                        assert_eq!(password, "password");
-                        Response::Success.write_to(&mut stream).unwrap();
-                    }
-                    req => panic!("wrong request: {:?}", req),
-                }
+                glib::timeout_future(Duration::from_millis(1000)).await;
+                gtk::main_quit();
+            }));
 
-                match Request::read_from(&mut stream).unwrap() {
-                    Request::StartSession { .. } => {
-                        Response::Success.write_to(&mut stream).unwrap();
-                        logged_in.store(true, Ordering::Relaxed);
-                    }
-                    req => panic!("wrong request: {:?}", req),
-                }
-            }
-        }));
+            gtk::main();
 
-        let wall_clock = WallClock::new();
-        wall_clock.set_default();
-        let shell = Shell::new();
-        shell.set_default();
-        shell.set_locked(true);
-
-        let vp = VirtualPointer::new(Connection::connect_to_env().unwrap());
-        let kb = VirtualKeyboard::new(Connection::connect_to_env().unwrap());
-
-        let ready_called = Arc::new(AtomicBool::new(false));
-        let (ready_tx, ready_rx) = async_channel::bounded(1);
-        shell.connect_ready(clone!(@strong ready_called => move |_| {
-            ready_called.store(true, Ordering::Relaxed);
-            ready_tx.send_blocking(()).expect("notify ready failed");
-        }));
-        glib::spawn_future_local(clone!(@weak shell => async move {
-            ready_rx.recv().await.unwrap();
-            glib::timeout_future(Duration::from_millis(500)).await;
-
-            // Move the mouse to where the main user selection should be.
-            // TODO: calculate this somehow?
-            // If we had access to the Gtk.Widget ref I think it's straightforward to determine
-            // absolute (screen) coords...
-            let (_, _, width, height) = shell.usable_area();
-            let width = width as u32;
-            let height = height as u32;
-            let x = width / 2;
-            let y = height / 2 - 25; // 25 pixel nudge upwards
-            vp.move_to(x, y, width, height).await;
-
-            // wait a few millis after mouse move - for improved observation in recordings
-            glib::timeout_future(Duration::from_millis(250)).await;
-            // then click the main user
-            vp.click();
-
-            // wait for keypad page to slide in
-            glib::timeout_future(Duration::from_millis(500)).await;
-
-            // type password (uh, literally)
-            let keys = [
-                KEY_P!(),
-                KEY_A!(),
-                KEY_S!(),
-                KEY_S!(),
-                KEY_W!(),
-                KEY_O!(),
-                KEY_R!(),
-                KEY_D!(),
-                KEY_ENTER!(),
-            ];
-            for key in keys {
-                kb.keypress(key).await;
-                glib::timeout_future(Duration::from_millis(100)).await;
-            }
-
-            glib::timeout_future(Duration::from_millis(1000)).await;
-            gtk::main_quit();
-        }));
-
-        gtk::main();
-
-        assert!(ready_called.load(Ordering::Relaxed));
-        assert!(logged_in.load(Ordering::Relaxed));
+            assert!(ready_called.load(Ordering::Relaxed));
+            assert!(logged_in.load(Ordering::Relaxed));
+        });
     }
 }
