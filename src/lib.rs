3dbf175b8a9b1689c332f18a86193258e121ee7f
Sam Day <me@samcday.com>
2024-08-29 13:58:29 +0200

    Only launch nested phoc if parent compositor does not implement phosh_private protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+use anyhow::{anyhow, Context};
 use gtk::{gdk, gio};
+use wayland_client::protocol::wl_registry;
 use crate::nested_phoc::NestedPhoc;
 
 mod keypad_shuffle;
@@ -12,11 +14,53 @@ mod user_session_page;
 
 pub const APP_ID: &str = "mobi.phosh.phrog";
 
-pub fn init(phoc: Option<String>) -> Option<NestedPhoc> {
-    gio::resources_register_include!("phrog.gresource").expect("Failed to register resources.");
+struct DetectPhoc (bool);
+
+impl wayland_client::Dispatch<wl_registry::WlRegistry, ()> for DetectPhoc {
+    fn event(
+        state: &mut Self,
+        _: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &wayland_client::Connection,
+        _: &wayland_client::QueueHandle<DetectPhoc>,
+    ) {
+        // When receiving events from the wl_registry, we are only interested in the
+        // `global` event, which signals a new available global.
+        // When receiving this event, we just print its characteristics in this example.
+        if let wl_registry::Event::Global { name, interface, version } = event {
+            if interface == "phosh_private" {
+                state.0 = true;
+            }
+        }
+    }
+}
+
+fn is_phoc_detected() -> anyhow::Result<bool> {
+    let conn = wayland_client::Connection::connect_to_env()?;
+
+    // Retrieve the WlDisplay Wayland object from the connection. This object is
+    // the starting point of any Wayland program, from which all other objects will
+    // be created.
+    let display = conn.display();
+
+    // Create an event queue for our event processing
+    let mut event_queue = conn.new_event_queue();
+    // And get its handle to associate new objects to it
+    let qh = event_queue.handle();
+    let _registry = display.get_registry(&qh, ());
+    let detect = &mut DetectPhoc(false);
+    event_queue.roundtrip(detect)?;
+    Ok(detect.0)
+}
+
+pub fn init(phoc: Option<String>) -> anyhow::Result<Option<NestedPhoc>> {
+    gio::resources_register_include!("phrog.gresource").context("failed to register resources.")?;
 
     let mut nested_phoc = if let Some(phoc_binary) = phoc {
-        Some(NestedPhoc::new(&phoc_binary))
+        if !is_phoc_detected().context("failed to detect Wayland compositor globals")? {
+            Some(NestedPhoc::new(&phoc_binary))
+        } else { None }
     } else { None };
 
     gdk::set_allowed_backends("wayland");
@@ -32,10 +76,10 @@ pub fn init(phoc: Option<String>) -> Option<NestedPhoc> {
     };
 
     if display.is_none() {
-        panic!("failed GDK init");
+        return Err(anyhow!("failed GDK init"));
     }
 
     gtk::init().unwrap();
     libhandy::init();
-    nested_phoc
+    Ok(nested_phoc)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use gtk::glib::Object;
 use gtk::Application;
 use libphosh::prelude::*;
 use libphosh::WallClock;
+use wayland_client::protocol::wl_registry;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "false",
-        help = "Launch nested phoc compositor"
+    #[arg(short, long, default_value = "true",
+        help = "Launch nested phoc compositor, if the current compositor is not already phoc"
     )]
     phoc: bool,
 
@@ -19,12 +20,14 @@ struct Args {
     fake: bool,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let mut args = Args::parse();
 
     // TODO: check XDG_RUNTIME_DIR here? Angry if not set? Default?
 
-    let _nested_phoc = phrog::init(if args.phoc { Some(String::from("phoc")) } else { None });
+    let _nested_phoc = phrog::init(if args.phoc {
+        Some(String::from("phoc"))
+    } else { None });
 
     let _app = Application::builder().application_id(phrog::APP_ID).build();
 
@@ -43,4 +46,6 @@ fn main() {
     shell.set_locked(true);
 
     gtk::main();
+
+    Ok(())
 }
