f2d707361fee117449f5b44b7da823955c92fe25
Sam Day <me@samcday.com>
2024-09-06 09:53:42 +0200

    build: Remove unnecessary schemas step in CI, remove homedir crate usage and fix Fedora builds probably

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ fn main() {
 
     let schema_file = "mobi.phosh.phrog.gschema.xml";
     let resources_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("resources");
-    let schema_path = homedir::my_home().unwrap().unwrap().join(".local/share/glib-2.0/schemas");
+    let schema_path = PathBuf::from(std::env::var("HOME").unwrap()).join(".local/share/glib-2.0/schemas");
     std::fs::create_dir_all(&schema_path).expect("failed to create schema dir");
 
     std::fs::write(&schema_path.join(schema_file),
